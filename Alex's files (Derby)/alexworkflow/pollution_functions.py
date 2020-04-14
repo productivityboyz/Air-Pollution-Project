e252cef0dd1d6c605cbded42955c6957b6cb84bf
Alex Large <alexanderklarge@gmail.com>
2020-04-14 17:51:10 +0100

    Added new plot of % of median and started plotting 10 lowest weeks of pollution on record

--- Alex's files (Derby)/alexworkflow/pollution_functions.py
+++ Alex's files (Derby)/alexworkflow/pollution_functions.py
@@ -1,84 +1,26 @@
-import os
-import pandas as pd
-from urllib.request import urlretrieve
-
-def get_data(location_input):
-    '''
-    Returns data for desired location
-    Parameters:
-    location - write a string of location wanted. Will only work with 'Derby' currently
-    '''
-    ### DICTIONARY OF DATA FROM DIFFERENT PLACES!
-    Derby = {
-        '2020' : 'https://uk-air.defra.gov.uk/data_files/site_data/DESA_2020.csv',
-        '2019' : 'https://uk-air.defra.gov.uk/data_files/site_data/DESA_2019.csv',
-        '2018' : 'https://uk-air.defra.gov.uk/data_files/site_data/DESA_2018.csv',
-        '2017' : 'https://uk-air.defra.gov.uk/data_files/site_data/DESA_2017.csv'
-    }
-    Reading = {
-        '2020' : 'https://uk-air.defra.gov.uk/data_files/site_data/REA5_2020.csv',
-        '2019' : 'https://uk-air.defra.gov.uk/data_files/site_data/REA5_2019.csv',
-        '2018' : 'https://uk-air.defra.gov.uk/data_files/site_data/REA5_2018.csv',
-        '2017' : 'https://uk-air.defra.gov.uk/data_files/site_data/REA5_2017.csv'
-    }
-    Nottingham_Centre = {
-        '2020' : 'https://uk-air.defra.gov.uk/data_files/site_data/NOTT_2020.csv',
-        '2019' : 'https://uk-air.defra.gov.uk/data_files/site_data/NOTT_2019.csv',
-        '2018' : 'https://uk-air.defra.gov.uk/data_files/site_data/NOTT_2018.csv',
-        '2017' : 'https://uk-air.defra.gov.uk/data_files/site_data/NOTT_2017.csv'
-    }
-
-    data_locations = {
-      "Derby" : Derby,
-      "Reading" : Reading,
-      "Nottingham_Centre" : Nottingham_Centre
-    }
-
-    global df_2020, df_2019, df_2018, df_2017
-    
-    URL_2020 = data_locations[location_input]['2020']
-    URL_2019 = data_locations[location_input]['2019']
-    URL_2018 = data_locations[location_input]['2018']
-    URL_2017 = data_locations[location_input]['2017']
-    
-    # Save URLs to .csv files
-    urlretrieve(URL_2020,'alex_data_2020.csv')
-    urlretrieve(URL_2019,'alex_data_2019.csv')
-    urlretrieve(URL_2018,'alex_data_2018.csv')
-    urlretrieve(URL_2017,'alex_data_2017.csv')
-    # Get dataframes from .csv files
-    df_2020 = pd.read_csv('alex_data_2020.csv',skiprows=4) # (skipping top 4 rows as they're empty)
-    df_2019 = pd.read_csv('alex_data_2019.csv',skiprows=4)
-    df_2018 = pd.read_csv('alex_data_2018.csv',skiprows=4)
-    df_2017 = pd.read_csv('alex_data_2017.csv',skiprows=4)
-    unit = 'ugm^3'
-    # Delete the .csv files
-    os.remove('alex_data_2020.csv')
-    os.remove('alex_data_2019.csv')
-    os.remove('alex_data_2018.csv')
-    os.remove('alex_data_2017.csv')
-    
-    return(df_2020,df_2019,df_2018,df_2017)
-    return('hello')
-
-def test_function():
-    print('hello')
-    
-    
 # function to let you decide date range to plot, resample type, line or bar, and aggregate type
-def plot_date_range(start,end,resample_string,plottype,aggregate):
-    from datetime import datetime
-    '''
-    Parameters:
-    start = date string in format YYYY-MM-DD
-    end = date string in format YYYY-MM-DD
-    resample_string = string of 'H'/'D'/'W'/'M'
-    plottype = string 'line' or 'bar'
-    aggregate = string 'sum','average','median'
-    '''
-    resample_string = resample_string.upper() # just in case user were to input h instead of H
+def date_range_plotter(df,start,end,resample_string,plottype,aggregate,cols,location_string):
     
-    # for title of chart
+    from datetime import datetime
+    import datetime as dt
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    import numpy as np
+    '''
+    Keyword arguments:
+    df -- df to use. Will probably always be df_all
+    start -- date string in format YYYY-MM-DD
+    end -- date string in format YYYY-MM-DD
+    resample_string -- string of 'H'/'D'/'W'/'M'
+    plottype -- string 'line' or 'bar'
+    aggregate -- string 'sum','average','median'
+    cols -- if 1, plot total. If 2, plot NO and NO2. If 3, plot total & NO & NO2. If 4, just NO. If 5, just NO2.
+    location_string -- string of location, so the title changes
+    '''
+    # Capitalise user input (in case user were to input 'h' instead of 'H')
+    resample_string = resample_string.upper()
+    
+    # For chart title 
     if resample_string == 'H':
         title_string = 'Hourly'
     elif resample_string == 'D':
@@ -87,194 +29,61 @@ def plot_date_range(start,end,resample_string,plottype,aggregate):
         title_string = 'Weekly'
     elif resample_string == 'M':
         title_string = 'Monthly'
+        
+    # Data to plot (from 'cols' parameter)
+    if cols == 1:
+        data_to_plot = ['Total']
+    elif cols == 2:
+        data_to_plot = ['Nitric oxide', 'Nitrogen dioxide']
+    elif cols == 3:
+        data_to_plot = ['Total', 'Nitric oxide', 'Nitrogen dioxide']
+    elif cols == 4:
+        data_to_plot = ['Nitric oxide']
+    elif cols == 5:
+        data_to_plot = ['Nitrogen dioxide']
     
-    # dates to plot 
+    # Dates to plot. Convert inputted string to datetime object with strptime
     start = datetime.strptime(start,'%Y-%m-%d')
     end = datetime.strptime(end,'%Y-%m-%d')
-    plt.style.use('seaborn')
-    # resampling based on input
+    
+    # Plot styling
+    plt.style.use('seaborn-poster')
+    
+    # Resampling data based on user input
     if resample_string == 'H':
-        z = df_all.loc[start:end]
+        df_ = df.loc[start:end]
         if plottype == 'line':
-            x = z[['Nitric oxide','Nitrogen dioxide']].plot(figsize=(15,10))
+            fig = df_[data_to_plot].plot(figsize=(15,10))
         elif plottype == 'bar':
-            x = z[['Nitric oxide','Nitrogen dioxide']].plot.bar(figsize=(15,10))
-        x.set_title('Hourly Emissions Between {} and {}'.format(start,end))
-        x.set_ylim(0)
+            fig = df_[data_to_plot].plot.bar(figsize=(15,10))
+        fig.set_title('Hourly Emissions Between {} and {}'.format(start,end))
+        fig.set_ylim(0)
     else:    
-        z = df_all.loc[start:end]
+        df_ = df.loc[start:end]
         if plottype == 'line':
             if aggregate == 'sum':
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).sum().plot(figsize=(15,10))
+                fig = df_[data_to_plot].resample(resample_string).sum().plot(figsize=(15,10))
             elif aggregate == 'mean':
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).mean().plot(figsize=(15,10))
+                fig = df_[data_to_plot].resample(resample_string).mean().plot(figsize=(15,10))
             elif aggregate == 'median': 
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).median().plot(figsize=(15,10))
-            x.set_title('{} {} Emissions Between {} and {} for {}'.format(aggregate.title(),title_string,start,end,location_string))
-            x.set_ylim(0)
+                fig = df_[data_to_plot].resample(resample_string).median().plot(figsize=(15,10))
+            fig.set_title('{} {} Emissions Between {} and {} for {}'.format(aggregate.title(),title_string,start,end,location_string))
+            fig.set_ylim(0)
         elif plottype == 'bar':
             if aggregate == 'sum':
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).sum().plot.bar(figsize=(15,10))
+                fig = df[data_to_plot].resample(resample_string).sum().plot.bar(figsize=(15,10))
             elif aggregate == 'mean':
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).mean().plot.bar(figsize=(15,10))
+                fig = df[data_to_plot].resample(resample_string).mean().plot.bar(figsize=(15,10))
             elif aggregate == 'median':
-                x = z[['Nitric oxide','Nitrogen dioxide']].resample(resample_string).median().plot.bar(figsize=(15,10))
-            x.set_title('{} {} Emissions Between {} and {} for {}'.format(aggregate.title(),title_string,start,end,location_string))
-            x.set_ylim(0)
-            
-            #plt.xticks(np.arange(3),['Jan 2020','Feb 2020','Mar 2020'])
-            #plt.xticks(rotation=1)
-            #plt.title('Monthly Emissions in 2020')
-            #plt.xlabel('')
-            plt.ylabel('Total Emissions Measured (ugm$^3$)')
-            plt.legend(loc='upper left')
-
-
-def year_comparison2(dfs = [df_2020,df_2019,df_2018,df_2017],start,end,resample_string,years_of_data):
-    global df_2020, df_2019, df_2018, df_2017
-    '''
-    Parameters:
-    start = string of date in format 'MM-DD'
-    end = string of date in format 'MM-DD'
-    resample_string = 'H','D','W','M'
-    years_of_data = 1, 2, 3 or 4, determines how many years to plot
-    '''
-   
-    # below is hideous but I'm just doing it in this dumb way because I know it'll work
-    # gets you start and end dates for each year
-    start_2020 = '2020-' + start
-    start_2020 = pd.to_datetime(start_2020)
-    end_2020 = '2020-' + end
-    end_2020 = pd.to_datetime(end_2020)
+                fig = df[data_to_plot].resample(resample_string).median().plot.bar(figsize=(15,10))
+            fig.set_title('{} {} Emissions Between {} and {} for {}'.
+                          format(aggregate.title(),title_string,start,end,location_string))
+            fig.set_ylim(0)
     
-    start_2019 = '2019-' + start
-    start_2019 = pd.to_datetime(start_2019)
-    end_2019 = '2019-' + end
-    end_2019 = pd.to_datetime(end_2019)
-       
-    start_2018 = '2018-' + start
-    start_2018 = pd.to_datetime(start_2018)
-    end_2018 = '2018-' + end
-    end_2018 = pd.to_datetime(end_2018)
-    
-    start_2017 = '2017-' + start
-    start_2017 = pd.to_datetime(start_2017)
-    end_2017 = '2017-' + end
-    end_2017 = pd.to_datetime(end_2017)
-    
-    ### MASKS ###
-    # 2020
-    mask = (df_2020['Date Time'] >= start_2020) & (df_2020['Date Time'] <= end_2020) # REPLACE MASKS WITH .LOC!
-    df_2020_ = df_2020.loc[mask]
-    # 2019
-    mask = (df_2019['Date Time'] >= start_2019) & (df_2019['Date Time'] <= end_2019) # REPLACE MASKS WITH .LOC!
-    df_2019_ = df_2019.loc[mask]
-    # 2018
-    mask = (df_2018['Date Time'] >= start_2018) & (df_2018['Date Time'] <= end_2018) # REPLACE MASKS WITH .LOC!
-    df_2018_ = df_2018.loc[mask]
-    # 2017
-    mask = (df_2017['Date Time'] >= start_2017) & (df_2017['Date Time'] <= end_2017) # REPLACE MASKS WITH .LOC!
-    df_2017_ = df_2017.loc[mask]
-    
-    if resample_string=='H':
-        ### Making date time no year column
-        df_2020_['Date Time No Year'] = df_2020_['Date Time'].dt.strftime('%m-%d %H:%M')
-        df_2019_['Date Time No Year'] = df_2019_['Date Time'].dt.strftime('%m-%d %H:%M')
-        df_2018_['Date Time No Year'] = df_2018_['Date Time'].dt.strftime('%m-%d %H:%M')
-        df_2017_['Date Time No Year'] = df_2017_['Date Time'].dt.strftime('%m-%d %H:%M')
-
-        ### Plotting them with date time no year
-        plt.figure(figsize=(15,10))
-        
-        if years_of_data==4:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                        label = '2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                        label = '2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-            sns.lineplot(x=df_2018_['Date Time No Year'],y=df_2018_['Nitric oxide'],
-                        label = '2018 Nitric Oxide',alpha=0.6,linestyle=':',color='#FF9914')
-            sns.lineplot(x=df_2017_['Date Time No Year'],y=df_2017_['Nitric oxide'],
-                        label ='2017 Nitrix Oxide',alpha=0.6,linestyle='-.',color='#EC4E20')
-        
-        elif years_of_data==3:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                    label = '2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                        label = '2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-            sns.lineplot(x=df_2018_['Date Time No Year'],y=df_2018_['Nitric oxide'],
-                        label = '2018 Nitric Oxide',alpha=0.6,linestyle=':',color='#FF9914')
-    
-        elif years_of_data==2:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                    label = '2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                        label = '2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-            
-        elif years_of_data==1:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                    label = '2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-    
-        plt.xticks(rotation=90) # would be great to strip the hours from the x ticks
-        plt.xlabel('')
-        plt.ylabel('Nitric oxide (ugm$^3$)')
-        plt.title('Hourly Total Nitric Oxide Emmisions From Date {} to {} of Each Year'.format(start,end))
-        plt.legend(loc='upper center')
-        plt.xticks('')
-    
-    elif resample_string!='H':
-        ### Resample accordingly (however only by day works)
-        plt.figure(figsize=(15,10))
-        df_2020_ = df_2020_.resample(resample_string).sum()
-        df_2019_ = df_2019_.resample(resample_string).sum()
-        df_2018_ = df_2018_.resample(resample_string).sum()
-        df_2017_ = df_2017_.resample(resample_string).sum()
-                
-        ### Making date time no year column
-        df_2020_['Date Time No Year'] = df_2020_.index.strftime('%m-%d %H:%M')
-        df_2019_['Date Time No Year'] = df_2019_.index.strftime('%m-%d %H:%M')
-        df_2018_['Date Time No Year'] = df_2018_.index.strftime('%m-%d %H:%M')
-        df_2017_['Date Time No Year'] = df_2017_.index.strftime('%m-%d %H:%M')
-        
-        #### Replacing date time as index as removing datatime column 
-        def cleaner(variable):
-            variable.index = variable['Date Time No Year']
-
-        cleaner(df_2020_)
-        cleaner(df_2019_)
-        cleaner(df_2018_)
-        cleaner(df_2017_)
-
-        plt.figure(figsize=(15,10))
-        if years_of_data==4:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                         label='2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                         label='2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-            sns.lineplot(x=df_2018_['Date Time No Year'],y=df_2018_['Nitric oxide'],
-                         label='2018 Nitric Oxide',alpha=0.6,linestyle=':',color='#FF9914')
-            sns.lineplot(x=df_2017_['Date Time No Year'],y=df_2017_['Nitric oxide'],
-                         label='2017 Nitric Oxide',alpha=0.6,linestyle='-.',color='#EC4E20')
-            
-        elif years_of_data==3:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                         label='2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                         label='2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-            sns.lineplot(x=df_2018_['Date Time No Year'],y=df_2018_['Nitric oxide'],
-                         label='2018 Nitric Oxide',alpha=0.6,linestyle=':',color='#FF9914')
-
-        elif years_of_data==2:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                         label='2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-            sns.lineplot(x=df_2019_['Date Time No Year'],y=df_2019_['Nitric oxide'],
-                         label='2019 Nitric Oxide',linestyle=':',alpha=0.6,color='#775253')
-
-        elif years_of_data==1:
-            sns.lineplot(x=df_2020_['Date Time No Year'],y=df_2020_['Nitric oxide'],
-                         label='2020 Nitric Oxide',linestyle='-',marker='o',alpha=1,color='#2C497F')
-
-        plt.xticks(rotation=90) # would be great to strip the hours from the x ticks
-        plt.xlabel('')
-        plt.ylabel('Nitric oxide (ugm$^3$)')
-        plt.title('Daily Total Nitric Oxide Emmisions From Date {} to {} of Each Year'.format(start,end))
-        plt.legend(loc='upper center')
+    # Stylings
+    fig.set_ylim(0,None)
+    fig.set_facecolor('#eaeaf2')
+    fig.grid(True,color='w',axis='y')
+    plt.ylabel('Total Emissions Measured (ugm$^3$)')
+    plt.legend(loc='upper center')
+    plt.xlabel('')
